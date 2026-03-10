958022df0c85a2bfedeef45b2c3d8ac22f416ec6
William Morris <wmorris@gurner.com.au>
2026-03-10 12:52:26 +1100

    update dashboard

---
+++ update_site.bat
@@ -0,0 +1,7 @@
+copy "C:\Users\WilliamMorris\OneDrive - Gurner TM\Projects - 189 Kent Street, Sydney\04. Legals\05. Off the Plan Contract of Sale\COS Tracker\189Kent_Dashboard.html" "C:\Users\WilliamMorris\189-Kent_Customisations\index.html"
+
+cd C:\Users\WilliamMorris\189-Kent_Customisations
+
+git add .
+git commit -m "update dashboard"
+git push
